d04a5b11ab4355c393d94315b981fca3a329851e
GalTechDev <poussigalitv@gmail.com>
2024-02-29 15:52:23 +0100

    fix x y z position

--- test/test_plotly.py
+++ test/test_plotly.py
@@ -1,20 +1,27 @@
 import plotly.graph_objects as plt
 import numpy as np
+import random
 
 #Visualisation très performente sur de nombreux points
 #Interface web
 
-res = 5
+res = 2
 d = 1
 
+min_h = 0
+max_h = 360
+
+min_v = 0
+max_v = 90 + res
+
 def gen_test_data():
-    for h in range(0, 360, res):
-        for v in range(0, 90, res):
-            yield (h,v,d)
+    for h in range(min_h, max_h, res):
+        for v in range(min_v, max_v+res, res):
+            yield (h,v, random.randint(1,1))
 
 def data2plot(h, v, d):
-    x = d*np.cos(np.deg2rad(h))
-    y = d*np.sin(np.deg2rad(h))
+    x = d*np.cos(np.deg2rad(h))*np.cos(np.deg2rad(v))
+    y = d*np.sin(np.deg2rad(h))*np.cos(np.deg2rad(v))
     z = d*np.sin(np.deg2rad(v))
 
     return (x, y, z)
@@ -24,16 +31,16 @@ def data2mesh(data_x, data_y, data_z):
     mesh_y = []
     mesh_z = []
     
-    coef = 90//res
+    coef = (max_v-min_v+res)//res
 
-    for hi in range(0, 360//res):
+    for hi in range(0, (max_h-min_h)//res):
         mesh_x.append(data_x[hi*coef:(hi+1)*coef])
         mesh_y.append(data_y[hi*coef:(hi+1)*coef])
         mesh_z.append(data_z[hi*coef:(hi+1)*coef])
     
     triangles = [] #[((x1, y1, z1), (x2, y2, z2), (x3, y3, z3)), ...] liste of tuple with 3 points that make shape
-    for hi in range(0, 360//res-1):
-        for vi in range(0, 90//res-1):
+    for hi in range(0, (max_h-min_h)//res-1):
+        for vi in range(0, (max_v-min_v)//res-1):
             triangles.append(
                 (
                     (mesh_x[hi][vi], mesh_y[hi][vi], mesh_z[hi][vi]), 
@@ -49,8 +56,8 @@ def data2mesh(data_x, data_y, data_z):
                     (mesh_x[hi-1][vi+1], mesh_y[hi-1][vi+1], mesh_z[hi-1][vi+1])
                 ))
             
-    for hi in range(0, 360//res):
-        for vi in range(1, 90//res):
+    for hi in range(0, (max_h-min_h)//res):
+        for vi in range(1, (max_v-min_v)//res):
             triangles.append(
                 (
                     (mesh_x[hi][vi], mesh_y[hi][vi], mesh_z[hi][vi]), 
@@ -83,5 +90,5 @@ mesh_x, mesh_y, mesh_z = data2mesh(data_x, data_y, data_z)
 
 m3d = plt.Mesh3d(x=mesh_x, y=mesh_y, z=mesh_z, i=list(range(0, len(mesh_x), 3)), j=list(range(1, len(mesh_x), 3)), k=list(range(2, len(mesh_x), 3)))
 m = plt.Scatter3d(x=data_x, y=data_y, z=data_z)
-fig = plt.Figure(data=[m])
+fig = plt.Figure(data=[m3d])
 fig.show()
